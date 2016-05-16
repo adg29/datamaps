3fda473790a5f29a4bc25d99721ecf083df545ac
Alan David Garcia <alandavidgarcia@gmail.com>
2016-05-16 03:47:34 -0400

    Build

--- src/js/datamaps.js
+++ src/js/datamaps.js
@@ -59,7 +59,10 @@
     },
     zoomConfig: {
         zoomOnClick: true,
-        zoomFactor: 0.8
+        zoomFactor: 0.8,
+        zoomCallback: function(d){
+          console.log('zoomCallback',d);
+        }
     },
     arcConfig: {
       strokeColor: '#DD1C77',
@@ -875,6 +878,8 @@ var asiaSelected = {
       .duration(750)
       .style("stroke-width", "1.5px")
       .attr('transform', 'scale(' + responsiveScale + ')')
+
+    zoomCallback(this);
   }
 
 
